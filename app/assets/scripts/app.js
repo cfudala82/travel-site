1cbc7669f654579bd3ac213ae9b233f42467a982
Christopher Fudala <cfudala82@me.com>
2018-03-01 19:46:05 -0600

    completed menu button

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,12 +1,3 @@
-var $ = require('jquery');
-var Person = require('./modules/Person');
+import MobileMenu from './modules/MobileMenu';
 
-alert("This is a test for our webpack automation,123");
-
-var john = new Person("John Doe", "blue");
-john.greet();
-
-var jane = new Person("Jane Doe", "green");
-jane.greet();
-
-$('h1').remove();
+var mobileMenu = new MobileMenu();
--- app/assets/scripts/modules/Person.js
+++
@@ -1,9 +0,0 @@
-function Person(fullName, favColor) {
-  this.fullName = fullName;
-  this.favColor = favColor;
-  this.greet = function() {
-    console.log("Hello, my name is " + this.fullName + " and my favorite color is " + this.favColor + ".");
-  }
-}
-
-module.exports = Person;
